29c9f2e11c86b1771043fefdab38851c761cf9a6
Roberto Fernandes <roberto.fernandes21@gmail.com>
2019-01-04 20:54:32 +0000

    Register activity

--- app/src/main/java/robfernandes/xyz/newchat/MainActivity.java
+++ app/src/main/java/robfernandes/xyz/newchat/MainActivity.java
@@ -1,7 +1,9 @@
 package robfernandes.xyz.newchat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -19,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initializeViews();
+        setClickListeners();
     }
 
     private void initializeViews() {
@@ -26,5 +29,16 @@ public class MainActivity extends AppCompatActivity {
         password = findViewById(R.id.activity_main_password);
         signUpText = findViewById(R.id.activity_main_sign_up_text);
         logInBtn = findViewById(R.id.activity_main_log_in_btn);
+    }
+
+
+    private void setClickListeners() {
+        signUpText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/robfernandes/xyz/newchat/RegisterActivity.java
+++ app/src/main/java/robfernandes/xyz/newchat/RegisterActivity.java
@@ -2,12 +2,33 @@ package robfernandes.xyz.newchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private TextView logInText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        initializeViews();
+        setClickListeners();
+    }
+
+    private void initializeViews() {
+        logInText = findViewById(R.id.activity_register_log_in_text);
+    }
+
+    private void setClickListeners() {
+        logInText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //finish activity and go back to log in
+                finish();
+            }
+        });
     }
 }
