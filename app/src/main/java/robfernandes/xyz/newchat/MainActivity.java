6411fbd7dae2381513417e16c3423d16a1997d9d
Roberto Fernandes <roberto.fernandes21@gmail.com>
2019-01-04 19:53:51 +0000

    Log In activity

--- app/src/main/java/robfernandes/xyz/newchat/MainActivity.java
+++ app/src/main/java/robfernandes/xyz/newchat/MainActivity.java
@@ -2,12 +2,29 @@ package robfernandes.xyz.newchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+    private TextView signUpText;
+    private Button logInBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeViews();
+    }
+
+    private void initializeViews() {
+        email = findViewById(R.id.activity_main_email);
+        password = findViewById(R.id.activity_main_password);
+        signUpText = findViewById(R.id.activity_main_sign_up_text);
+        logInBtn = findViewById(R.id.activity_main_log_in_btn);
     }
 }
