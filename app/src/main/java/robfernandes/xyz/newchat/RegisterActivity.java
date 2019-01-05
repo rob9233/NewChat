a075083b41f6a3aed204d7bc6f957388ebe7e618
Roberto Fernandes <roberto.fernandes21@gmail.com>
2019-01-05 13:54:51 +0000

    User register

--- app/src/main/java/robfernandes/xyz/newchat/RegisterActivity.java
+++ app/src/main/java/robfernandes/xyz/newchat/RegisterActivity.java
@@ -1,12 +1,19 @@
 package robfernandes.xyz.newchat;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -15,12 +22,15 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText emailEditText;
     private EditText passwordEditText;
     private Button registerBtn;
+    private FirebaseAuth mAuth;
+    private static final String TAG = RegisterActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        mAuth = FirebaseAuth.getInstance();
         initializeViews();
         setClickListeners();
     }
@@ -64,7 +74,28 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void registerUser(String username, String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            displayToast("Register succeed");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            displayToast("Register failed, please try again");
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
 
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            //update ui
+        }
     }
 
     private void displayToast(String message) {
