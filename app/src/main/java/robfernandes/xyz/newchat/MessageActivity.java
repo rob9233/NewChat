f0a59c2fdacd7ecf737041cfbd5c104be115c402
Roberto Fernandes <roberto.fernandes21@gmail.com>
2019-01-08 15:05:12 +0000

    user sign out

--- app/src/main/java/robfernandes/xyz/newchat/MessageActivity.java
+++ app/src/main/java/robfernandes/xyz/newchat/MessageActivity.java
@@ -1,13 +1,32 @@
 package robfernandes.xyz.newchat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MessageActivity extends AppCompatActivity {
+
+    private TextView testTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+
+        testTextView = findViewById(R.id.activity_message_test);
+        testTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(MessageActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                startActivity(intent);
+            }
+        });
     }
 }
