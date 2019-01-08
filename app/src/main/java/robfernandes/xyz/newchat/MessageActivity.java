2ae14378b68d2a1820a4559d5b7d20eb13e6aa7c
Roberto Fernandes <roberto.fernandes21@gmail.com>
2019-01-08 15:26:58 +0000

    user menu

--- app/src/main/java/robfernandes/xyz/newchat/MessageActivity.java
+++ app/src/main/java/robfernandes/xyz/newchat/MessageActivity.java
@@ -1,9 +1,10 @@
 package robfernandes.xyz.newchat;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -18,15 +19,37 @@ public class MessageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_message);
 
         testTextView = findViewById(R.id.activity_message_test);
-        testTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(MessageActivity.this, MainActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-
-                startActivity(intent);
-            }
-        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.user_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.user_menu_contacts_item:
+            goToContacts();
+            break;
+            case R.id.user_menu_log_out_item:
+            logOut();
+            break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void goToContacts() {
+        testTextView.setText("Contacts!!");
+    }
+
+    private void logOut() {
+        FirebaseAuth.getInstance().signOut();
+
+        Intent intent = new Intent(MessageActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
 }
