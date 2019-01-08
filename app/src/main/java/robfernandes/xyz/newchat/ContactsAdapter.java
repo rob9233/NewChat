c44661abcc348ef4f64eec19cd2df96ca31d4d76
Roberto Fernandes <roberto.fernandes21@gmail.com>
2019-01-08 17:48:08 +0000

    Contacts activity

---
+++ app/src/main/java/robfernandes/xyz/newchat/ContactsAdapter.java
@@ -0,0 +1,33 @@
+package robfernandes.xyz.newchat;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Roberto Fernandes on 08/01/2019.
+ */
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public ContactsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactsAdapter.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
