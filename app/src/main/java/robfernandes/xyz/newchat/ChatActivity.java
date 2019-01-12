b111357ce28d24c7997a6a0bee209b13f479d28e
Roberto Fernandes <roberto.fernandes21@gmail.com>
2019-01-12 18:41:41 +0000

    wip chat messages adapter

--- app/src/main/java/robfernandes/xyz/newchat/ChatActivity.java
+++ app/src/main/java/robfernandes/xyz/newchat/ChatActivity.java
@@ -1,13 +1,29 @@
 package robfernandes.xyz.newchat;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class ChatActivity extends AppCompatActivity {
+    private ChatAdapter mChatAdapter;
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        setRecyclerView();
+    }
+
+    private void setRecyclerView() {
+           mChatAdapter = new ChatAdapter();
+        mChatAdapter.addMessage(true, "estou a enviar");
+        mChatAdapter.addMessage(true, "estou a enviar outra");
+        mChatAdapter.addMessage(false, "estou a enviar receber");
+            mRecyclerView = findViewById(R.id.activity_chat_recycler_view);
+            mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+            mRecyclerView.setAdapter(mChatAdapter);
     }
 }
--- app/src/main/java/robfernandes/xyz/newchat/ChatAdapter.java
+++ app/src/main/java/robfernandes/xyz/newchat/ChatAdapter.java
@@ -1,7 +1,67 @@
 package robfernandes.xyz.newchat;
 
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Roberto Fernandes on 12/01/2019.
  */
-public class ChatAdapter {
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
+    private List<Boolean> isSendingList = new ArrayList<>();
+    private List<String> messageList = new ArrayList<>();
+
+    public void addMessage(boolean isSending, String message) {
+        isSendingList.add(isSending);
+        messageList.add(message);
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        int layout;
+
+        if (i == 0) {
+            layout = R.layout.message_sending_layout;
+        } else {
+            layout = R.layout.message_receiving_layout;
+        }
+
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(layout, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (isSendingList.get(position)) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if (isSendingList!=null) {
+            return isSendingList.size();
+        }
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
